fb04c2b7e1dd5bfaa3e029151cdd265a4cc20d1d
tylergreason <tyler.greason@protonmail.com>
2020-08-29 13:50:00 -0400

    palindrome

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// palindrom 
+// palindrome 
+const palindrome = string => {
+    // turn string lowercase 
+    string = string.toLowerCase()
+    // iterate through letters of word and see if the current letter is the same as its mirror 
+    string.split('').forEach((letter, i)=>{
+        if (i < Math.floor(string.length/2)){
+            if (letter === string[string.length - (i + 1)]){
+                console.log(letter + ' ' + string[string.length - (i +1)] + ' ' + true)
+            }else{
+                console.log(letter + ' ' + string[string.length - (i +1)] + ' ' + false)
+                return false
+            }
+        }
+    })
+}
+console.log('aNne');
+palindrome('anne')
+console.log('apple');
+palindrome('apple')
+console.log('abbe');
+palindrome('abbe')
+
+const fizzBuzz = n => {
+    
+}
