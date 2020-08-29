3479ea5f9590ae475e8a30d6550b29cd5fc09d8a
tylergreason <tyler.greason@protonmail.com>
2020-08-29 14:19:49 -0400

    anogram

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const palindrome = string => {
         }
     })
 }
-console.log('aNne');
-palindrome('anne')
-console.log('apple');
-palindrome('apple')
-console.log('abbe');
-palindrome('abbe')
+// console.log('aNne');
+// palindrome('anne')
+// console.log('apple');
+// palindrome('apple')
+// console.log('abbe');
+// palindrome('abbe')
 
 const fizzBuzz = n => {
     for (let i = 0; i <= n; i++){
@@ -39,4 +39,41 @@ const fizzBuzz = n => {
     }
 }
 
-fizzBuzz(15)
+// fizzBuzz(15)
+
+const anagram = (word1, word2) => {
+    let returnValue; 
+    //  first check their lengths 
+    if (word1.length === word2.length){ 
+        let object1 = wordObject(word1); 
+        let object2 = wordObject(word2); 
+        for (let letter in object1){
+            if (returnValue !== false){
+                if (object1[letter] === object2[letter]){
+                    returnValue = true; 
+                }else {
+                    returnValue = false;
+                }
+            }
+        }
+    }else{
+        returnValue = false 
+    }
+    console.log(returnValue);
+    return returnValue; 
+}
+
+// function to count the letters in a word and make objects out of their quantities 
+const wordObject = word => {
+    word.toLowerCase()
+    let returnObject = {}; 
+    for (let i = 0; i < word.length; i++){
+        // check if letter is in returnObject yet, and if not add it 
+        if (returnObject[word[i]]){
+            returnObject[word[i]] += 1;
+        }else {
+            returnObject[word[i]] = 1; 
+        }
+    }
+    return returnObject;
+}
